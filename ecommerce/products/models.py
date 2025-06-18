88921fb3c2f49c3f27428102e595a38cc1d961b0
AnshMNSoni <anshsoni702@gmail.com>
2025-06-18 17:19:08 +0530

    added product details into the database

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -7,10 +7,10 @@ class Category(models.Model):
         return self.name
     
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    category = models.CharField(max_length=150, default=None)
     name = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField(upload_to="products/")
+    image = models.ImageField(upload_to="products/images/")
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
     is_available = models.BooleanField(default=True)
